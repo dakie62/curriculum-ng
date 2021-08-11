214979e2d604e542017c8b3ef9cd195cdb522408
dakie62 <aebrard62730@gmail.com>
2021-08-11 20:45:03 +0200

    feat: adding technologies images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './section/about-me/about-me.component';
+import { SkillsComponent } from './section/skills/skills.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
